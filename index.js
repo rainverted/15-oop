b336e7541894b718c84c74d787d1e900d8e46470
rainverted <MY_NAME@example.com>
2021-05-21 17:34:41 +0300

    Dog class

---
+++ index.js
@@ -0,0 +1,7 @@
+const Dog = require('./js/Dog');
+
+const rexas = new Dog('Rex', 'brown');
+const spaikas = new Dog('Spike', 'white');
+
+console.log(rexas.furColor);
+console.log(spaikas.furColor);
---
+++ js/Dog.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name, fur) {
+        this.name = name;
+        this.furColor = fur;
+        this.legsCount = 4;
+        this.hasTale = true;
+
+    }
+}
+
+module.exports = Dog;    //node back.end panudojimas vietoj export 
